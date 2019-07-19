7ced728c1d20fe3b8d49a49d848f645f1f602c54
康良玉 <kangly@game7.cc>
2019-07-19 11:16:07 +0800

    Hello World

---
+++ src/main/java/com/caogen/springboothello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.caogen.springboothello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String HelloWord() {
+        return "Hello World!";
+    }
+
+}
