c4102ad763cb97b5d70ed9b6a390cf5c59a003f4
康良玉 <kangly@game7.cc>
2019-07-22 16:49:58 +0800

    test

--- deleteSpringBoot-Hello.sh
+++ deleteSpringBoot-Hello.sh
@@ -1,8 +1,10 @@
-IMAGE_NAME=registry.cn-shenzhen.aliyuncs.com/caogen/springboot_hello
+IMAGE_NAME=registry.cn-shenzhen.aliyuncs.com/caogen/springboot-hello
 IID=$(docker images | grep "$IMAGE_NAME" | awk '{print $3}')
 run(){
     if [ -n "$IID" ]; then
+        echo "exist $IMAGE_NAME image，IID=$IID, ,delete docker image ..."
         docker rmi -f $IID
+        echo "$IMAGE_NAME image delete success"
     fi
 }
 run
